6b7cc608531546b19e49e00c6f7b0d4c4ca0ab6c
carissacks <46070698+carissacks@users.noreply.github.com>
2019-04-08 11:33:48 +0700

    Padding live stream

--- page-live.php
+++ page-live.php
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+<?php get_header(); ?>
+    
+<?php ## STREAM CONTENT ## ?>
+<div class="container">
+        
+    <div class="row justify-content-center" id="contentTitle">
+        <div class="col-12  align-self-center">
+            <h1 class="streamtitle">Live Stream</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-11 col-md-12 embed-responsive embed-responsive-16by9 m-md-4 m-2">
+            <iframe class="latestVideoEmbed embed-responsive-item" cid="UC2I_6GJyyXHzVzuXvp9IsfA" frameborder="0" allowfullscreen style="margin-bottom:50em;"></iframe>
+        </div>
+    </div>
+    
+</div>
+<?php ## END OF STREAM CONTENT ## ?>
+
+
+=======
 <?php get_header(); ?>
     
 <?php ## STREAM CONTENT ## ?>
@@ -17,4 +39,5 @@
 <?php ## END OF STREAM CONTENT ## ?>
 
 
+>>>>>>> 4324c36b1e49212f9d12610c3d2906d8f10c3861
 <?php get_footer(); ?>
