eef8e25d15e9599cc81c36e6f93a6e4ea90c83d9
unknown <carissa.cks@gmail.com>
2019-04-13 12:28:25 +0700

    remove duplicated directory

--- televisiumn/page-about.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<?php ## ABOUT US CONTENT ## ?>
-<div id="about-content" class="container-fluid">
-    <a href="#" class="effect-shine">TEZT</a>
-</div>
-<?php ## END OF ABOUT US CONTENT ## ?>
-
-<?php get_footer(); ?>
